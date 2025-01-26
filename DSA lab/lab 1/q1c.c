5d9728478c323c7dbb37a61c971f7e2b5316736b
nsh07 <66912344+nsh07@users.noreply.github.com>
2025-01-26 19:54:11 +0530

    Fix algorithm failing if first element was max or min element

--- DSA lab/lab 1/q1c.c
+++ DSA lab/lab 1/q1c.c
@@ -1,30 +1,27 @@
+#include <limits.h>
 #include <stdio.h>
 
 int main() {
-    int arr[5] = {3,2,4,1,5};
-    int min = arr[0];
-    int secmin = arr[0];
-    int max = arr[0];
-    int secmax = arr[0];
-    for (int i = 0; i < 5; i++) {
+    int arr[10] = {11, 8, 3, 2, 5, 6, 4, 9, 7, 10};
+    int min = INT_MAX, secmin = INT_MAX;
+    int max = INT_MIN, secmax = INT_MIN;
+
+    for (int i = 0; i < 10; i++) {
         if (arr[i] < min) {
             secmin = min;
             min = arr[i];
         } else if (arr[i] < secmin) {
             secmin = arr[i];
         }
-       
+
         if (arr[i] > max) {
             secmax = max;
             max = arr[i];
         } else if (arr[i] > secmax) {
             secmax = arr[i];
         }
-       
-        
     }
-    printf("%d\n",secmin);
-    printf("%d\n",secmax);
+    printf("%d\n%d\n", secmin, secmax);
 
     return 0;
 }
